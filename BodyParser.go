afdea773abc3de0236d7ae71b332c7e611f56c96
Adam Presley <adam@adampresley.com>
2015-02-07 15:19:37 -0600

    Added function to check if a request contains Content-Type with a value of application/json

--- BodyParser.go
+++ BodyParser.go
@@ -14,10 +14,24 @@ function will fill in the values from the request JSON
 into any matching struct fields, or will hydrate them
 into a map if possible.
 */
-func ParseJsonBody(request *http.Request, receiver interface{}) (error) {
+func ParseJsonBody(request *http.Request, receiver interface{}) error {
 	var err error
 
 	body, _ := ioutil.ReadAll(request.Body)
 	err = json.Unmarshal(body, receiver)
 	return err
+}
+
+/*
+Returns true/false if the request contains a Content-Type
+header, and if that header is set to "application/json".
+*/
+func RequestIsJsonContentType(request *http.Request) bool {
+	headers := request.Header
+
+	if headers.Get("Content-Type") == "application/json" {
+		return true
+	} else {
+		return false
+	}
 }
